58ee58d74cd62826be5047306b567466ebc074a6
justkira <shighgamerhg@gmail.com>
2025-05-25 08:33:30 +0300

    chore: Replace FlyCamera with PanOrbitCamera and add mesh rendering

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,66 @@
-use bevy::prelude::*;
-use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
+use bevy::{
+    prelude::*,
+    render::{
+        mesh::{Indices, VertexAttributeValues},
+        render_asset::RenderAssetUsages,
+        render_resource::PrimitiveTopology,
+    },
+};
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_plugins(FlyCameraPlugin)
+        .add_plugins(PanOrbitCameraPlugin)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn((Camera3d::default(), FlyCamera::default()));
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Setup Camera
+
+    let mesh_handle: Handle<Mesh> = meshes.add(create_mesh_handle());
+    let uv_texture = asset_server.load("textures/uv_checker.png");
+
+    commands.spawn((
+        Mesh3d(mesh_handle),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color_texture: Some(uv_texture),
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 1.0, 0.0),
+    ));
+
+    commands.spawn((
+        Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
+        PanOrbitCamera::default(),
+    ));
+    commands.spawn((PointLight::default(), Transform::from_xyz(1.8, 1.8, 1.8)));
+}
+
+fn create_mesh_handle() -> Mesh {
+    Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        vec![
+            [-1.0, -1.0, 0.0],
+            [-1.0, 1.0, 0.0],
+            [1.0, -1.0, 0.0],
+            [1.0, 1.0, 0.0],
+        ],
+    )
+    .with_computed_flat_normals()
+    .with_inserted_attribute(
+        Mesh::ATTRIBUTE_UV_0,
+        vec![[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]],
+    )
+    .with_inserted_indices(Indices::U32(vec![0, 1, 2, 1, 3, 2]))
 }
