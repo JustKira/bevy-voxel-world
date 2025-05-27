1c9957750c4f7d0b7535c14b82602fef61e8d0cc
justkira <shighgamerhg@gmail.com>
2025-05-27 19:19:08 +0300

    feat: Add 3D chunk generation and improve voxel mesh rendering

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 mod mesher;
 
 use bevy::{
-    color::palettes::{css::WHITE, tailwind::RED_500},
+    color::palettes::{css, tailwind},
     pbr::wireframe::{WireframeConfig, WireframePlugin},
     prelude::*,
-    render::{mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology},
 };
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
-use mesher::mesh_data::create_mesh;
+use mesher::mesh_data::create_chunk;
 
 fn main() {
     App::new()
@@ -17,7 +16,7 @@ fn main() {
         .add_plugins(PanOrbitCameraPlugin)
         .insert_resource(WireframeConfig {
             global: true,
-            default_color: RED_500.into(),
+            default_color: tailwind::GRAY_700.into(),
         })
         .run();
 }
@@ -26,84 +25,45 @@ fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    asset_server: Res<AssetServer>,
+    // asset_server: Res<AssetServer>,
 ) {
     // Setup Camera
 
-    let mesh_handle: Handle<Mesh> = meshes.add(create_mesh());
-    let uv_texture = asset_server.load("textures/uv_checker.png");
+    // let uv_texture = asset_server.load("textures/uv_checker.png");
 
     commands.spawn((
-        Mesh3d(mesh_handle),
+        Mesh3d(meshes.add(create_chunk(32, 32, 32))),
         // MeshMaterial3d(materials.add(StandardMaterial {
         //     base_color_texture: Some(uv_texture.clone()),
         //     ..default()
         // })),
-        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: css::WHITE.into(),
+            ..default()
+        })),
         Transform::from_xyz(0.0, 0.0, 0.0),
     ));
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color_texture: Some(uv_texture),
-            ..default()
-        })),
-        Transform::from_xyz(0.0, 3.5, 0.0),
-    ));
+    // commands.spawn((
+    //     Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+    //     MeshMaterial3d(materials.add(StandardMaterial {
+    //         base_color_texture: Some(uv_texture),
+    //         ..default()
+    //     })),
+    //     Transform::from_xyz(0.0, 3.5, 0.0),
+    // ));
 
     commands.spawn((
         Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
         PanOrbitCamera::default(),
     ));
-    commands.spawn((PointLight::default(), Transform::from_xyz(1.8, 1.8, 1.8)));
-}
 
-fn create_mesh_handle() -> Mesh {
-    Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-    )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vec![
-            //Bottom Face
-            [0.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            //Top Face
-            [0.0, 1.0, 0.0],
-            [1.0, 1.0, 0.0],
-            [1.0, 1.0, 1.0],
-            [0.0, 1.0, 1.0],
-        ],
-    )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            // Bottom face
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-            [0.0, 1.0],
-            // Top face
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-            [0.0, 1.0],
-            // Front face
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 1.0],
-            [0.0, 1.0],
-        ],
-    )
-    .with_inserted_indices(Indices::U32(vec![
-        0, 1, 2, 2, 3, 0, // Bottom Face
-        4, 7, 5, 5, 7, 6, // Top Face
-        0, 5, 1, 5, 0, 4, // Front Face
-    ]))
-    .with_computed_flat_normals()
-    .with_computed_normals()
+    commands.spawn((
+        DirectionalLight {
+            illuminance: DirectionalLight::default().illuminance * 0.05,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(-4.0, 2.0, 0.0).looking_at(Vec3::NEG_X * 1.5, Vec3::Y),
+    ));
 }
--- src/mesher/mesh_data.rs
+++ src/mesher/mesh_data.rs
@@ -1,5 +1,6 @@
 use bevy::{
     asset::RenderAssetUsages,
+    math::{Vec3, vec3},
     render::mesh::{Indices, Mesh, PrimitiveTopology},
 };
 
@@ -9,16 +10,15 @@ use bevy::{
 // The X axis goes from left to right (+X points right)
 // The Y axis goes from bottom to top (+Y points up).
 // The Z axis goes from far to near (+Z points towards you, out of the screen).
-
-const VOXEL_VERTICES: [[f32; 3]; 8] = [
-    [0.0, 0.0, 0.0], // Vert 0
-    [1.0, 0.0, 0.0], // Vert 1
-    [1.0, 0.0, 1.0], // Vert 2
-    [0.0, 0.0, 1.0], // Vert 3
-    [0.0, 1.0, 0.0], // Vert 4
-    [1.0, 1.0, 0.0], // Vert 5
-    [1.0, 1.0, 1.0], // Vert 6
-    [0.0, 1.0, 1.0], // Vert 7
+const VOXEL_VERTICES: [Vec3; 8] = [
+    vec3(0.0, 0.0, 0.0), // Vert 0
+    vec3(1.0, 0.0, 0.0), // Vert 1
+    vec3(1.0, 0.0, 1.0), // Vert 2
+    vec3(0.0, 0.0, 1.0), // Vert 3
+    vec3(0.0, 1.0, 0.0), // Vert 4
+    vec3(1.0, 1.0, 0.0), // Vert 5
+    vec3(1.0, 1.0, 1.0), // Vert 6
+    vec3(0.0, 1.0, 1.0), // Vert 7
 ];
 
 // quad order
@@ -27,15 +27,30 @@ const QUAD_VERTICES: [[u8; 4]; 6] = [
     // Note the Diagonal are always shared we can Group them together
     [3, 2, 1, 0], // -> DOWN
     [4, 5, 6, 7], // -> UP
-    [0, 4, 7, 3], // -> RIGHT
-    [2, 6, 5, 1], // -> LEFT
+    [2, 6, 5, 1], // -> RIGHT
+    [0, 4, 7, 3], // -> LEFT
     [3, 7, 6, 2], // -> FRONT
     [1, 5, 4, 0], // -> BACK
 ];
 
+const VOXEL_NORMALS: [[f32; 3]; 6] = [
+    [0.0, -1.0, 0.0], // DOWN
+    [0.0, 1.0, 0.0],  // UP
+    [1.0, 0.0, 0.0],  // RIGHT
+    [-1.0, 0.0, 0.0], // LEFT
+    [0.0, 0.0, 1.0],  // FRONT
+    [0.0, 0.0, -1.0], // BACK
+];
+
 // We are created Seperated Quad for each Face with there own Vertices
 // Beacuse we need Flat faces and We will use **Greedy mesher**!
-fn create_quad(indices: &mut Vec<u32>, vertices: &mut Vec<[f32; 3]>, side: usize) {
+fn create_quad(
+    indices: &mut Vec<u32>,
+    vertices: &mut Vec<[f32; 3]>,
+    normals: &mut Vec<[f32; 3]>,
+    side: usize,
+    offset: Vec3,
+) {
     let vertices_count = vertices.len() as u32;
 
     indices.push(vertices_count + 1);
@@ -46,27 +61,21 @@ fn create_quad(indices: &mut Vec<u32>, vertices: &mut Vec<[f32; 3]>, side: usize
     indices.push(vertices_count + 3);
 
     for vertex in 0..4 as usize {
-        let index = QUAD_VERTICES[side][vertex];
-        let position = VOXEL_VERTICES[QUAD_VERTICES[side][vertex] as usize];
-        vertices.push(position);
+        let position = VOXEL_VERTICES[QUAD_VERTICES[side][vertex] as usize] + offset;
+        vertices.push(position.to_array());
+        normals.push(VOXEL_NORMALS[side]);
     }
-}
-
-// We are creating a Voxel Mesh with 6 Quads
-fn create_voxel() -> (Vec<u32>, Vec<[f32; 3]>) {
-    let mut indices: Vec<u32> = Vec::new();
-    let mut vertices: Vec<[f32; 3]> = Vec::new();
-
-    // loop to create Each Quad
-    for side in 0..6 {
-        create_quad(&mut indices, &mut vertices, side);
-    }
-
-    (indices, vertices)
 }
 
 pub fn create_mesh() -> Mesh {
-    let (indices, vertices) = create_voxel();
+    let mut indices: Vec<u32> = Vec::new();
+    let mut vertices: Vec<[f32; 3]> = Vec::new();
+    let mut normals: Vec<[f32; 3]> = Vec::new();
+
+    // loop to create Each Quad
+    for side in 0..6 {
+        create_quad(&mut indices, &mut vertices, &mut normals, side, Vec3::ZERO);
+    }
 
     Mesh::new(
         PrimitiveTopology::TriangleList,
@@ -74,5 +83,34 @@ pub fn create_mesh() -> Mesh {
     )
     .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
     .with_inserted_indices(Indices::U32(indices))
-    .with_computed_normals()
+    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
+}
+
+pub fn create_chunk(x_size: u32, y_size: u32, z_size: u32) -> Mesh {
+    let mut indices: Vec<u32> = Vec::new();
+    let mut vertices: Vec<[f32; 3]> = Vec::new();
+    let mut normals: Vec<[f32; 3]> = Vec::new();
+    for x in 0..x_size {
+        for y in 0..y_size {
+            for z in 0..z_size {
+                for side in 0..6 {
+                    create_quad(
+                        &mut indices,
+                        &mut vertices,
+                        &mut normals,
+                        side,
+                        Vec3::new(x as f32, y as f32, z as f32),
+                    );
+                }
+            }
+        }
+    }
+
+    Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_indices(Indices::U32(indices))
+    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
 }
