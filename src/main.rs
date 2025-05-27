d899986fa2e224fe5d6939d1c01b55a8e684b2de
justkira <shighgamerhg@gmail.com>
2025-05-27 18:25:37 +0300

    feat: Add mesher module and refactor mesh creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod mesher;
+
 use bevy::{
     color::palettes::{css::WHITE, tailwind::RED_500},
     pbr::wireframe::{WireframeConfig, WireframePlugin},
@@ -5,6 +7,8 @@ use bevy::{
     render::{mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology},
 };
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+
+use mesher::mesh_data::create_mesh;
 
 fn main() {
     App::new()
@@ -26,24 +30,27 @@ fn setup(
 ) {
     // Setup Camera
 
-    let mesh_handle: Handle<Mesh> = meshes.add(create_mesh_handle());
+    let mesh_handle: Handle<Mesh> = meshes.add(create_mesh());
     let uv_texture = asset_server.load("textures/uv_checker.png");
 
     commands.spawn((
         Mesh3d(mesh_handle),
+        // MeshMaterial3d(materials.add(StandardMaterial {
+        //     base_color_texture: Some(uv_texture.clone()),
+        //     ..default()
+        // })),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color_texture: Some(uv_texture),
             ..default()
         })),
-        // MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.0, 0.0),
+        Transform::from_xyz(0.0, 3.5, 0.0),
     ));
-
-    // commands.spawn((
-    //     Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-    //     MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-    //     Transform::from_xyz(0.0, 0.5, 0.0),
-    // ));
 
     commands.spawn((
         Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
@@ -60,24 +67,43 @@ fn create_mesh_handle() -> Mesh {
     .with_inserted_attribute(
         Mesh::ATTRIBUTE_POSITION,
         vec![
-            // Back face (Z = 0)
+            //Bottom Face
             [0.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0], // 1: bottom-right-back
-            [1.0, 0.0, 1.0], // 2: top-right-back
-            [0.0, 0.0, 1.0], // 3: top-left-back
+            [1.0, 0.0, 0.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0],
+            //Top Face
+            [0.0, 1.0, 0.0],
+            [1.0, 1.0, 0.0],
+            [1.0, 1.0, 1.0],
+            [0.0, 1.0, 1.0],
         ],
     )
     .with_inserted_attribute(
         Mesh::ATTRIBUTE_UV_0,
         vec![
-            // Front face - corrected UV (flipped vertically)
-            [0.0, 1.0],
-            [1.0, 1.0],
-            [1.0, 0.0],
+            // Bottom face
             [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.0, 1.0],
+            // Top face
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.0, 1.0],
+            // Front face
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.0, 1.0],
         ],
     )
-    .with_inserted_indices(Indices::U32(vec![0, 1, 2, 2, 3, 0]))
-    .with_duplicated_vertices()
+    .with_inserted_indices(Indices::U32(vec![
+        0, 1, 2, 2, 3, 0, // Bottom Face
+        4, 7, 5, 5, 7, 6, // Top Face
+        0, 5, 1, 5, 0, 4, // Front Face
+    ]))
     .with_computed_flat_normals()
+    .with_computed_normals()
 }
---
+++ src/mesher/mesh_data.rs
@@ -0,0 +1,88 @@
+use bevy::{
+    asset::RenderAssetUsages,
+    render::mesh::{Indices, Mesh, PrimitiveTopology},
+};
+
+// Check Bevy Coordinate System
+// https://bevy-cheatbook.github.io/fundamentals/coords.html
+//
+// The X axis goes from left to right (+X points right)
+// The Y axis goes from bottom to top (+Y points up).
+// The Z axis goes from far to near (+Z points towards you, out of the screen).
+
+const VOXEL_VERTICES: [[f32; 3]; 8] = [
+    [0.0, 0.0, 0.0], // Vert 0
+    [1.0, 0.0, 0.0], // Vert 1
+    [1.0, 0.0, 1.0], // Vert 2
+    [0.0, 0.0, 1.0], // Vert 3
+    [0.0, 1.0, 0.0], // Vert 4
+    [1.0, 1.0, 0.0], // Vert 5
+    [1.0, 1.0, 1.0], // Vert 6
+    [0.0, 1.0, 1.0], // Vert 7
+];
+
+// quad order
+// down, up, right, left, front, back
+const QUAD_VERTICES: [[u8; 4]; 6] = [
+    // Note the Diagonal are always shared we can Group them together
+    [3, 2, 1, 0], // -> DOWN
+    [4, 5, 6, 7], // -> UP
+    [0, 4, 7, 3], // -> RIGHT
+    [2, 6, 5, 1], // -> LEFT
+    [3, 7, 6, 2], // -> FRONT
+    [1, 5, 4, 0], // -> BACK
+];
+
+// We are created Seperated Quad for each Face with there own Vertices
+// Beacuse we need Flat faces and We will use **Greedy mesher**!
+fn create_quad(indices: &mut Vec<u32>, vertices: &mut Vec<[f32; 3]>, side: usize) {
+    let vertices_count = vertices.len() as u32;
+
+    indices.push(vertices_count + 1);
+    indices.push(vertices_count + 0);
+    indices.push(vertices_count + 2);
+    indices.push(vertices_count + 2);
+    indices.push(vertices_count + 0);
+    indices.push(vertices_count + 3);
+
+    println!(
+        "Triangles (last 6 points): {:?}",
+        &indices[indices.len().saturating_sub(6)..]
+    );
+    println!("Vertices Len {}", vertices.len());
+
+    for vertex in 0..4 as usize {
+        let index = QUAD_VERTICES[side][vertex];
+        let position = VOXEL_VERTICES[QUAD_VERTICES[side][vertex] as usize];
+        println!(
+            "Side Vert({}) , Index({}) : Position {:#?}",
+            vertex, index, position
+        );
+        vertices.push(position);
+    }
+}
+
+// We are creating a Voxel Mesh with 6 Quads
+fn create_voxel() -> (Vec<u32>, Vec<[f32; 3]>) {
+    let mut indices: Vec<u32> = Vec::new();
+    let mut vertices: Vec<[f32; 3]> = Vec::new();
+
+    // loop to create Each Quad
+    for side in 0..6 {
+        create_quad(&mut indices, &mut vertices, side);
+    }
+
+    (indices, vertices)
+}
+
+pub fn create_mesh() -> Mesh {
+    let (indices, vertices) = create_voxel();
+
+    Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_indices(Indices::U32(indices))
+    .with_computed_normals()
+}
